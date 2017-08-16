bdf5e06885bfdea8f4c333451ff342147fe6609a
williamchans <williamchans57@gmail.com>
2017-08-16 17:15:01 +0700

    Add existing file

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Greeting.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Greeting.java
@@ -2,15 +2,15 @@ package com.example.bot.spring;
 
 public class Greeting {
 
-    private final long id;
+    private final String id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public Greeting(String id, String content) {
         this.id = id;
         this.content = content;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
