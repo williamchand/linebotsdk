4a7364475def055a51f6c262ed421516421c868a
williamchans <williamchans57@gmail.com>
2017-08-16 20:04:27 +0700

    Add existing file

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/MainConfig.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/MainConfig.java
@@ -3,56 +3,13 @@ package com.example.bot.spring;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import java.util.Map;
-import javax.sql.DataSource;
+
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.*;
-import java.util.ArrayList;
 
 @Configuration
 @ComponentScan(basePackages = "com.example.bot.spring")
-public class MainConfig {
-
-	@Autowired
-	private DataSource dataSource;
-
-	@RequestMapping("/db")
-	String db(Map<String, Object> model) {
-	    try (Connection connection = dataSource.getConnection()) {
-	      Statement stmt = connection.createStatement();
-	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-	      ArrayList<String> output = new ArrayList<String>();
-	      while (rs.next()) {
-	        output.add("Read from DB: " + rs.getTimestamp("tick"));
-	      }
-
-	      model.put("records", output);
-	      return "db";
-	    } catch (Exception e) {
-	      model.put("message", e.getMessage());
-	      return "error";
-	    }
-	}
-}
-import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
-@Configuration
-@ComponentScan(basePackages = "com.heroku.example")
 public class MainConfig {
 
     @Bean
