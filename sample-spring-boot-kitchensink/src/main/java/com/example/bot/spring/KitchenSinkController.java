3e70c81d277604c1c25bac115bd33bc056a7a87c
williamchans <williamchans57@gmail.com>
2017-08-16 21:23:23 +0700

    Add existing file

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -293,6 +293,18 @@ public class KitchenSinkController {
         			  "feature /help : bantuan\n"+"/imagemap:gambar yang dapat diklik\n"+"/buttons:tombol\n"+
 		    		  "/question:pertanyaan\n"+"/carousel:carousel\n"+"/leave:keluar dari grup\n"+"/profile:user ID\n");
 	  }else if(text.indexOf("/time")>=0){
+		  try{
+	    		Statement stmt = dataSource.getConnection().createStatement();
+	        	stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+	        	stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+	        	stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+	        	ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+	        	while (rs.next()) {
+	        		this.replyText(replyToken,"Read from DB: " + rs.getTimestamp("tick"));
+	        	}
+	    	}catch(SQLException e){
+	    		this.replyText(replyToken,e.getMessage());
+	        }
 
 	  }else{
                 log.info("Ignore message {}: {}", replyToken, text);
