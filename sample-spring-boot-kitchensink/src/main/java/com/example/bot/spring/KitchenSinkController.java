6c0fc391ffa9316f27018f0ed218a1b0c45ba2b6
williamchans <williamchans57@gmail.com>
2017-08-17 17:30:03 +0700

    Add Existing Files

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -302,7 +302,7 @@ public class KitchenSinkController {
 		  	        Statement stmt = connection.createStatement();
 		  	        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
 		  	        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-		  	        stmt.executeUpdate("INSERT INTO ticks VALUES (now() + INTERVAL 7 HOUR)");
+		  	        stmt.executeUpdate("INSERT INTO ticks VALUES (now() + INTERVAL '7 HOUR')");
 		  	        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 		  	        while (rs.next()) {
 		  	        	this.replyText(replyToken,"Read from DB: " + rs.getTimestamp("tick"));
@@ -375,28 +375,9 @@ public class KitchenSinkController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="UserId", defaultValue="") String User,@RequestParam(value="name", defaultValue="") String name) {
-    	this.pushText(User,name);
+  
         return new Greeting(User,
                             String.format(template, name));
     }
-    private void push(@NonNull String To, @NonNull List<Message> messages) {
-        try {
-            BotApiResponse apiResponse = lineMessagingClient
-                    .pushMessage(new PushMessage(To, messages))
-                    .get();
-            log.info("Sent messages: {}", apiResponse);
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void pushText(@NonNull String To, @NonNull String message) {
-        if (To.isEmpty()) {
-            throw new IllegalArgumentException("To must not be empty");
-        }
-        if (message.length() > 1000) {
-            message = message.substring(0, 1000 - 2) + "â€¦â€¦";
-        }
-        this.push(To, new TextMessage(message));
-    }
+ 
 }
