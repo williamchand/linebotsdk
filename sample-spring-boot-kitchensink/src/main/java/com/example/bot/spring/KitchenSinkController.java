1aefd1f42b7c7376f1e59936fb88f04109a8072d
williamchans <williamchans57@gmail.com>
2017-08-31 14:25:11 +0700

    add existing files

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -468,8 +468,9 @@ public class KitchenSinkController {
   	         	        			stmt.executeUpdate("INSERT INTO \"tabel Jawaban\" (\"Jawaban\",\"GroupId\") VALUES ('"+rs2.getString("Jawaban")+"','"+groupId+"')");	
   	    	         	   			this.pushText(groupId,DisplayName+" Berhasil menjawab");
   	  	         	        		this.pushText(groupId,""+ rs2.getString("Pertanyaan"));
-	         	        			stmt.executeUpdate("UPDATE \"Tabel Skor\" SET \"Skor\" = \"Skor\"+1 WHERE \"GroupId\" = '"+groupId+"' AND \"UserId\" = '"+ userId + "' EXISTS (SELECT * FROM \"Tabel Skor\" WHERE \"GroupId\" = '"+groupId+"' AND \"UserId\" = '"+ userId + "')");
-	         	        			stmt.executeUpdate("INSERT INTO \"Tabel Skor\" (\"UserId\",\"GroupId\",\"Skor\") VALUES('"+userId+"','"+groupId+"',1) WHERE NOT EXISTS (SELECT * FROM \"Tabel Skor\" WHERE \"GroupId\" = '"+groupId+"' AND \"UserId\" = '"+ userId + "')");
+	         	        			stmt.executeUpdate("UPDATE \"Tabel Skor\" SET \"Skor\" = \"Skor\"+1 WHERE \"GroupId\" = '"+groupId+"' AND \"UserId\" = '"+ userId + "' EXISTS (SELECT * FROM \"Tabel Skor\" WHERE \"GroupId\" = '"+groupId+"' AND \"UserId\" = '"+ userId + "')");	
+  	    	         	   			this.pushText(groupId,DisplayName+" Berhasil menjawab");
+	         	        			stmt.executeUpdate("INSERT INTO \"Tabel Skor\" (\"UserId\",\"GroupId\",\"Skor\") SELECT ('"+userId+"','"+groupId+"',1) FROM \"Tabel Skor\" WHERE NOT EXISTS (SELECT * FROM \"Tabel Skor\" WHERE \"GroupId\" = '"+groupId+"' AND \"UserId\" = '"+ userId + "')");
 
   		  	         	        }
   	        					rs2.close();
